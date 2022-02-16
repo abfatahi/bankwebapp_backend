167f09e844242070f2cff53133a535207692a29f
abfatahi <abfatahi.iaf@gmail.com>
2022-02-16 23:17:49 +0100

    Feat: transfer schema & model

---
+++ database/models/Transfer.js
@@ -0,0 +1,30 @@
+import mongoose from 'mongoose';
+
+const TransferSchema = new mongoose.Schema(
+  {
+    transactionId: {
+      type: String,
+    },
+    sender: {
+      type: String,
+    },
+    receiver: {
+      type: String,
+    },
+    amount: {
+      type: Number,
+    },
+    status: {
+      type: String,
+      default: 'pending',
+    },
+    currency: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+const TransferModel = mongoose.model('TransferModel', TransferSchema);
+
+export default UserModel;
