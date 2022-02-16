d251a5eba6d9151cdaf4fd290589f7d975d167f7
abfatahi <abfatahi.iaf@gmail.com>
2022-02-16 23:11:07 +0100

    Feat: user model & schema

---
+++ database/models/User.js
@@ -0,0 +1,63 @@
+import mongoose from 'mongoose';
+
+const UserSchema = new mongoose.Schema(
+  {
+    fullName: {
+      type: String,
+      trim: true,
+      lowercase: true,
+    },
+    email: {
+      type: String,
+      trim: true,
+      unique: true,
+      lowercase: true,
+    },
+    phoneNumber: {
+      type: String,
+      trim: true,
+    },
+    password: {
+      type: String,
+      trim: true,
+    },
+    accountNumber: {
+      type: String,
+      trim: true,
+    },
+    bookBalance: {
+      type: Number,
+    },
+    USDBalance: {
+      type: Number,
+      default: 1000,
+    },
+    EURBalance: {
+      type: Number,
+      default: 0.0,
+    },
+    NGNBalance: {
+      type: Number,
+      default: 0.0,
+    },
+    availableBalance: {
+      type: Number,
+    },
+    authCode: {
+      type: Number,
+      trim: true,
+    },
+    beneficiaries: {
+      type: Array,
+    },
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const UserModel = mongoose.model('UserModel', UserSchema);
+
+export default UserModel;
